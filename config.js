deeb432cb7905af7ffe02841a47941220c55adb3
VADLURIVIDHATH <111587094+VADLURIVIDHATH@users.noreply.github.com>
2023-07-31 20:58:50 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+
+// add SDK Firebase
+var firebaseConfig = {
+
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.database();
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAlnKQ5AyeWLPCzw_mrWbbVtHr7TSdF6cY",
+    authDomain: "open-source-9a179.firebaseapp.com",
+    projectId: "open-source-9a179",
+    storageBucket: "open-source-9a179.appspot.com",
+    messagingSenderId: "874181981595",
+    appId: "1:874181981595:web:f87ce48b68ab3c5755dd3e"
+  };
+  
